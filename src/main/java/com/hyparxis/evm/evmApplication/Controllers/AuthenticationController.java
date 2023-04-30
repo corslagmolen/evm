1bd5c59d1e3da1c0b74bac8340b9388622a88863
Cor Slagmolen <cor.slagmolen@gmail.com>
2023-04-30 19:15:54 +0200

    Logging in Authentication controller

--- src/main/java/com/hyparxis/evm/evmApplication/Controllers/AuthenticationController.java
+++ src/main/java/com/hyparxis/evm/evmApplication/Controllers/AuthenticationController.java
@@ -5,6 +5,8 @@ import com.hyparxis.evm.evmApplication.auth.AuthenticationRequest;
 import com.hyparxis.evm.evmApplication.auth.AuthenticationResponse;
 import com.hyparxis.evm.evmApplication.auth.RegisterRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+    private final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ){
+        LOGGER.info("Inside Register of AuthenticationController");
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
